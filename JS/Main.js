3dd00b6dfe81ae59c8e4a4287be111a03e70200f
Warren <warrener.ryan@gmail.com>
2026-09-07 10:19:20 -0400

    More work on overhaul v108.

--- JS/Main.js
+++ JS/Main.js
@@ -263,3 +263,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateThemeIcons();
 });
+
+
+const cursorSmall = document.querySelector('.small');
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+
+    cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+    cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+}
+
+window.addEventListener('mousemove', positionElement)
